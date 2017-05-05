eb634df4b08b2f538449271ce1681e7c210eb986
Tha1n <alx.quettier@gmail.com>
2017-05-05 17:17:48 +0200

    Change object event due to deprecated tag

--- src/main/java/huxley/listeners/MessageListener.java
+++ src/main/java/huxley/listeners/MessageListener.java
@@ -6,10 +6,10 @@ import org.slf4j.LoggerFactory;
 import huxley.commands.ICommand;
 import huxley.model.discord.DiscordClient;
 import sx.blah.discord.api.events.EventSubscriber;
-import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
 
 /**
- * TODO --> Adding Comment
+ * A Message Listener for all Discord message.
  * Created by alxqu on 24/04/2017.
  */
 public class MessageListener {
@@ -18,7 +18,7 @@ public class MessageListener {
 
     @EventSubscriber
     public void onReady(MessageReceivedEvent event) {
-        // If the author is a bot, message get ignored
+        // If the author is a bot, message is ignored
         if (!event.getMessage().getAuthor().isBot()) {
             for(ICommand command : DiscordClient.getCommands()) {
                 command.request(event.getMessage());
